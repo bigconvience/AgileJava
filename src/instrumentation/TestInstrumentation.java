5dc69a851688a74f4fab4368e7012a754ca54a06
本朋 <benpeng.jbp@alibaba-inc.com>
2016-05-12 13:24:02 +0800

    add instrumentation

---
+++ src/instrumentation/TestInstrumentation.java
@@ -0,0 +1,9 @@
+package instrumentation;
+
+
+public class TestInstrumentation {
+    public static void main(String args[]) throws InterruptedException {
+        Lion l = new Lion();
+        l.runLion();
+    }
+}
