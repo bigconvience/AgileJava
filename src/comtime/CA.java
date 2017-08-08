c8480d69be9aa2c5df44fc0221f74747b897671e
本朋 <benpeng.jbp@alibaba-inc.com>
2017-08-08 11:44:15 +0800

    add enum

---
+++ src/comtime/CA.java
@@ -0,0 +1,10 @@
+package comtime;
+
+/**
+ * Created by jiangbenpeng on 21/06/2017.
+ *
+ * @author benpeng.jiang
+ * @version 1.0.0
+ */
+public class CA {
+}
---
+++ src/comtime/Main.java
@@ -0,0 +1,12 @@
+package comtime;
+
+/**
+ * Created by jiangbenpeng on 21/06/2017.
+ *
+ * @author benpeng.jiang
+ * @version 1.0.0
+ */
+public class Main {
+    public static void main(String args[]) {
+        CA ca = new CA() ; }
+}
